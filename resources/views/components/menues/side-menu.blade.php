1228cf6285a82d50f469843ecc3553676bc0adaf
asmi046 <asmi046@gmail.com>
2026-01-29 14:03:02 +0300

    + меню на мобильной версии

---
+++ resources/views/components/menues/side-menu.blade.php
@@ -0,0 +1,19 @@
+<div id="main_side_menue" class="side_menue_wrapper">
+    <div class="shadow"></div>
+    <nav class="side_menue">
+        <div class="inner_wrapper">
+            {{-- <x-burger-icon class="active_force"></x-burger-icon> --}}
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('img/logo.svg') }}" alt="Эстедент Плюс">
+            </a>
+            <div class="main_menu_puncts">
+                <x-menues.puncts></x-menues.puncts>
+            </div>
+
+        </div>
+    </nav>
+</div>
+
+<div class="show_menue_button">
+    <x-burger-icon></x-burger-icon>
+</div>
